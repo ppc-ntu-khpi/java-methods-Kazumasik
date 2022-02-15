a30f851a3493f7b152f15d6be859ee206d3793a3
Maxim <57413985+Kazumasik@users.noreply.github.com>
2022-02-15 13:38:35 +0200

    Update Exercise.java

--- src/Exercise.java
+++ src/Exercise.java
@@ -8,10 +8,10 @@ public class Exercise {
         Date nowDate = dateFormat.parse(dateFormat.format(new Date()));
         Date birthDate = dateFormat.parse(userDate);
         int diffInDays = (int)( (nowDate.getTime() - birthDate.getTime())/ (1000 * 60 * 60 * 24) );
-        System.out.println("Значення ваших віборитмів на сьогодні, на: "+ nowDate);
-        System.out.println("Физ. состояние:" + Math.sin(2*Math.PI*diffInDays/23));
-        System.out.println("Эмоциональное состояние:" + Math.sin(2*Math.PI*diffInDays/28));
-        System.out.println("Интелектуальное состояние:" + Math.sin(2*Math.PI*diffInDays/33));
+        System.out.println("Значення ваших біоритмів на сьогодні, на: "+ nowDate);
+        System.out.println("Фізичний стан:" + Math.sin(2*Math.PI*diffInDays/23));
+        System.out.println("Емоційний стан: " + Math.sin(2*Math.PI*diffInDays/28));
+        System.out.println("Інтелектуальний стан: " + Math.sin(2*Math.PI*diffInDays/33));
       }
       catch(Exception ex){
          System.out.println(ex);
